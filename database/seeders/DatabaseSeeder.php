4e25019a3a8ef1866306135c7bf1121885cebed3
bmorales993 <66859710+bmorales993@users.noreply.github.com>
2023-08-28 18:14:20 -0500

    new file:   app/Http/Livewire/Navigation.php 	new file:   app/Models/Category.php 	new file:   app/Models/Image.php 	new file:   app/Models/Post.php 	new file:   app/Models/Tag.php 	modified:   app/Models/User.php 	modified:   config/filesystems.php 	new file:   database/factories/CategoryFactory.php 	new file:   database/factories/ImageFactory.php 	new file:   database/factories/PostFactory.php 	new file:   database/factories/TagFactory.php 	new file:   database/migrations/2023_08_17_124446_create_categories_table.php 	new file:   database/migrations/2023_08_25_122736_create_posts_table.php 	new file:   database/migrations/2023_08_25_123600_create_tags_table.php 	new file:   database/migrations/2023_08_25_123808_create_post_tag_table.php 	new file:   database/migrations/2023_08_25_124054_create_images_table.php 	modified:   database/seeders/DatabaseSeeder.php 	new file:   database/seeders/PostSeeder.php 	new file:   database/seeders/UserSeeder.php 	modified:   resources/views/layouts/app.blade.php 	new file:   resources/views/livewire/navigation.blade.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +22,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Storage::deleteDirectory('posts'); //elimina la carpeta
+        Storage::makeDirectory('posts');
+
+       $this->call(UserSeeder::class);
+       Category::factory(4)->create();
+       Tag::factory(8)->create();
+       $this->call(PostSeeder::class);
+
     }
 }
